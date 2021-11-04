17ecb20e380e92ada1c90fc7a280e9f1718f011c
Daniel Shaal <daniel.shaal@s.afeka.ac.il>
2021-11-04 19:30:50 +0200

    Added AdminController

---
+++ src/main/java/controllers/AdminController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import boundaries.ActivityBoundary;
+import boundaries.UserBoundary;
+
+@RestController
+public class AdminController {
+
+	@RequestMapping(path = "/iob/admin/users/{userDomain}/{userEmail}", method = RequestMethod.DELETE)
+	public void deleteUsers() {
+		System.err.println("DELETE USERS");
+	}
+
+	@RequestMapping(path = "/iob/admin/instances/{userDomain}/{userEmail}", method = RequestMethod.DELETE)
+	public void deleteInstances() {
+		System.err.println("DELETE INSTANCES");
+	}
+
+	@RequestMapping(path = "/iob/admin/activities/{userDomain}/{userEmail}", method = RequestMethod.DELETE)
+	public void deleteActivities() {
+		System.err.println("DELETE ACTIVITIES");
+	}
+
+	@RequestMapping(path = "/iob/admin/users/{userDomain}/{userEmail}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public UserBoundary[] getUsers() {
+		UserBoundary arr[] = new UserBoundary[2];
+		arr[0] = new UserBoundary(null, "role-1", "username-1", "avatar-1");
+		arr[1] = new UserBoundary(null, "role-2", "username-2", "avatar-2");
+		return arr;
+	}
+
+	@RequestMapping(path = "/iob/admin/activities/{userDomain}/{userEmail}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ActivityBoundary[] getActivities() {
+		ActivityBoundary arr[] = new ActivityBoundary[2];
+		arr[0] = new ActivityBoundary(null, "type-1", null, null);
+		arr[1] = new ActivityBoundary(null, "type-2", null, null);
+		return arr;
+	}
+
+}
