017ea4bd470ae3b7738c91c1a656b82a6879aa9a
Daniel <Daniel@DESKTOP-4UAQC0T>
2021-12-19 17:19:06 +0200

    Rewinding Nir's changes

--- src/main/java/iob/attributes/InstanceId.java
+++ src/main/java/iob/attributes/InstanceId.java
@@ -1,16 +1,15 @@
 package iob.attributes;
 
 import java.io.Serializable;
-import java.util.Objects;
 
-public class InstanceId implements Serializable {
+public class InstanceId implements Serializable{
 	private static final long serialVersionUID = -238147502679170859L;
 	private String domain;
 	private String id;
 
 	public InstanceId() {
 	}
-
+	
 	public InstanceId(String domain, String id) {
 		this.domain = domain;
 		this.id = id;
@@ -30,23 +29,6 @@ public class InstanceId implements Serializable {
 
 	public void setId(String id) {
 		this.id = id;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(domain, id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		InstanceId other = (InstanceId) obj;
-		return Objects.equals(domain, other.domain) && Objects.equals(id, other.id);
 	}
 
 }
