df28fb4bb1cbcf4253c5ef1c1d782e3658d7450a
Daniel <Daniel@DESKTOP-4UAQC0T>
2021-12-22 21:09:27 +0200

    Permission - player won't get inactive instances

--- src/main/java/iob/aspects/PermissionsAspect.java
+++ src/main/java/iob/aspects/PermissionsAspect.java
@@ -1,17 +1,24 @@
 package iob.aspects;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import iob.annotations.RolePermission;
+import iob.boundaries.InstanceBoundary;
 import iob.data.UserRole;
+import iob.errors.NotFoundException;
 import iob.errors.UnauthorizedAccessException;
 import iob.logic.UsersServiceJpa;
 
@@ -22,6 +29,14 @@ public class PermissionsAspect {
 	@Autowired
 	private UsersServiceJpa userService;
 
+	public static List<?> convertObjectToList(Object obj) {
+		List<?> list = new ArrayList<>();
+		if (obj instanceof Collection) {
+			list = new ArrayList<>((Collection<?>) obj);
+		}
+		return list;
+	}
+
 	@Around("@annotation(iob.annotations.RolePermission)")
 	public Object permissionProxy(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 		Method method = ((MethodSignature) proceedingJoinPoint.getSignature()).getMethod();
@@ -30,6 +45,7 @@ public class PermissionsAspect {
 
 		Object args[] = proceedingJoinPoint.getArgs();
 		String domain = "", email = "";
+
 		if (args.length >= 2) {
 			domain = "" + args[0];
 			email = "" + args[1];
@@ -39,6 +55,27 @@ public class PermissionsAspect {
 		if (Arrays.asList(accessRole).contains(userRole)) {
 			try {
 				Object retVal = proceedingJoinPoint.proceed();
+
+				if (retVal instanceof Collection) {
+					List<?> list = convertObjectToList(retVal);
+					try {
+						if (userRole == UserRole.PLAYER && list.get(0) instanceof InstanceBoundary) {
+
+							retVal = list.stream().filter(instance -> ((InstanceBoundary) instance).getActive())
+									.collect(Collectors.toList());
+						} else {
+
+						}
+					} catch (NullPointerException e) {
+						return retVal;
+					} catch (Exception e) {
+
+					}
+				} else {
+					if (retVal instanceof InstanceBoundary) {
+						throw new NotFoundException("Instance doesn't exist or inactive");
+					}
+				}
 				return retVal;
 			} catch (Throwable e) {
 				throw e;
