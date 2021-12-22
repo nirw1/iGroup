f2c11554483b091b3e09198120f7a24241d1b778
Daniel <Daniel@DESKTOP-4UAQC0T>
2021-12-22 17:39:53 +0200

    Changed folder name again

--- src/test/java/admin_role_tests/AdminActivitiesTests.java
+++ src/test/java/admin_role_tests/AdminActivitiesTests.java
@@ -1,4 +1,4 @@
-package adminRoleTests;
+package admin_role_tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,15 +18,16 @@ import org.springframework.web.client.RestTemplate;
 
 import iob.Application;
 import iob.attributes.UserId;
+import iob.boundaries.ActivityBoundary;
 import iob.boundaries.NewUserBoundary;
 import iob.boundaries.UserBoundary;
-import iob.data.InstanceEntity;
+import iob.data.ActivityEntity;
 import iob.data.UserRole;
 import iob.logic.TestingDaoService;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
 @Profile("Testing")
-public class AdminInstancesTests {
+public class AdminActivitiesTests {
 
 	@Autowired
 	private TestingDaoService testingService;
@@ -45,39 +46,39 @@ public class AdminInstancesTests {
 
 	@PostConstruct
 	public void postConstruct() {
-		this.url = "http://localhost:" + this.port + "/iob/admin/instances/";
+		this.url = "http://localhost:" + this.port + "/iob/admin/activities/";
 		this.createUserUrl = "http://localhost:" + this.port + "/iob/users/";
 		this.client = new RestTemplate();
 	}
 
 	@BeforeEach
-	public void createInstance() {
-		InstanceEntity entity = new InstanceEntity();
-		entity.setId("1");
+	public void createActivity() {
+		ActivityEntity entity = new ActivityEntity();
+		entity.setId(1);
 		entity.setDomain("DOMAIN");
-		this.testingService.getInstanceDao().save(entity);
+//		entity.setInvokedBy("USER");
+		this.testingService.getActivityDao().save(entity);
 	}
 
-//
 	@AfterEach
-	public void deleteAllInstancesAndUsers() {
+	public void deleteAllActivitiesAndUsers() {
 		this.testingService.getUserDao().deleteAll();
-		this.testingService.getInstanceDao().deleteAll();
+		this.testingService.getActivityDao().deleteAll();
 	}
 
 	@Test
-	public void testAdminDeleteAllInstances() {
+	public void testAdminDeleteAllActivities() {
 		this.user = this.client
 				.postForObject(this.createUserUrl,
 						new NewUserBoundary("admin@mail.com", UserRole.ADMIN, "admin", "admin"), UserBoundary.class)
 				.getUserId();
 
 		this.client.delete(this.url + this.user);
-		assertThat(this.testingService.getInstanceDao().findAll()).hasSize(0);
+		assertThat(this.testingService.getActivityDao().findAll()).hasSize(0);
 	}
 
 	@Test
-	public void testManagerDeleteAllInstances() {
+	public void testManagerDeleteAllActivities() {
 		this.user = this.client.postForObject(this.createUserUrl,
 				new NewUserBoundary("manager@mail.com", UserRole.MANAGER, "manager", "manager"), UserBoundary.class)
 				.getUserId();
@@ -88,7 +89,7 @@ public class AdminInstancesTests {
 	}
 
 	@Test
-	public void testPlayerDeleteAllInstances() {
+	public void testPlayerDeleteAllActivities() {
 		this.user = this.client
 				.postForObject(this.createUserUrl,
 						new NewUserBoundary("player@mail.com", UserRole.PLAYER, "player", "player"), UserBoundary.class)
@@ -96,6 +97,38 @@ public class AdminInstancesTests {
 
 		assertThrows(HttpClientErrorException.Forbidden.class, () -> {
 			this.client.delete(this.url + this.user);
+		});
+	}
+
+	@Test
+	public void testAdminGetAllActivities() {
+		this.user = this.client
+				.postForObject(this.createUserUrl,
+						new NewUserBoundary("admin@mail.com", UserRole.ADMIN, "admin", "admin"), UserBoundary.class)
+				.getUserId();
+		assertThat(this.client.getForObject(this.url + this.user, ActivityBoundary[].class)).hasSizeGreaterThan(0);
+	}
+
+	@Test
+	public void testManagerGetAllActivities() {
+		this.user = this.client.postForObject(this.createUserUrl,
+				new NewUserBoundary("manager@mail.com", UserRole.MANAGER, "manager", "manager"), UserBoundary.class)
+				.getUserId();
+
+		assertThrows(HttpClientErrorException.Forbidden.class, () -> {
+			this.client.getForObject(this.url + this.user, ActivityBoundary[].class);
+		});
+	}
+
+	@Test
+	public void testPlayerGetAllActivities() {
+		this.user = this.client
+				.postForObject(this.createUserUrl,
+						new NewUserBoundary("player@mail.com", UserRole.PLAYER, "player", "player"), UserBoundary.class)
+				.getUserId();
+
+		assertThrows(HttpClientErrorException.Forbidden.class, () -> {
+			this.client.getForObject(this.url + this.user, ActivityBoundary[].class);
 		});
 	}
 }
