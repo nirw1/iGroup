9fda7429ed0f8a1d28d57ec71e215dcad2cfad5d
Daniel <Daniel@DESKTOP-4UAQC0T>
2021-12-23 17:08:36 +0200

    Added api tests & Testing factory

---
+++ src/main/java/iob/logic/TestingFactory.java
@@ -0,0 +1,114 @@
+package iob.logic;
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import iob.attributes.CreatedBy;
+import iob.attributes.Instance;
+import iob.attributes.InstanceId;
+import iob.attributes.Location;
+import iob.attributes.UserId;
+import iob.boundaries.ActivityBoundary;
+import iob.boundaries.InstanceBoundary;
+import iob.boundaries.NewUserBoundary;
+import iob.boundaries.UserBoundary;
+import iob.data.UserRole;
+
+@Component
+@Profile("Testing")
+public class TestingFactory {
+	private RestTemplate client;
+	private AtomicLong id;
+	private String createUserUrl;
+	private String createInstanceUrl;
+	private String createActivityUrl;
+
+	public TestingFactory() {
+		this.id = new AtomicLong();
+		this.client = new RestTemplate();
+	}
+
+	public void setPort(int port) {
+		this.createUserUrl = "http://localhost:" + port + "/iob/users";
+		this.createInstanceUrl = "http://localhost:" + port + "/iob/instances/";
+		this.createActivityUrl = "http://localhost:" + port + "/iob/activities/";
+	}
+
+	public UserBoundary createNewUser(UserRole role) {
+		NewUserBoundary tmp = new NewUserBoundary();
+		tmp.setAvatar("" + this.id.get());
+		tmp.setRole(role);
+		tmp.setEmail("" + this.id.get() + "@mail.com");
+		tmp.setUsername("" + this.id.get());
+		this.id.incrementAndGet();
+		return this.client.postForObject(this.createUserUrl, tmp, UserBoundary.class);
+	}
+
+	public InstanceBoundary createNewInstance(UserId createdBy, boolean isActive) {
+		InstanceBoundary tmp = new InstanceBoundary();
+		tmp.setActive(isActive);
+		tmp.setCreatedBy(new CreatedBy(createdBy));
+		tmp.setCreatedTimestamp(new Date());
+		tmp.setLocation(new Location(this.id.get(), this.id.get()));
+		tmp.setName("" + this.id.get());
+		tmp.setType("" + this.id.get());
+		this.id.incrementAndGet();
+		return this.client.postForObject(this.createInstanceUrl + createdBy, tmp, InstanceBoundary.class);
+	}
+
+	public ActivityBoundary createNewActivity(InstanceId instanceId, UserId invokedBy) {
+		ActivityBoundary tmp = new ActivityBoundary();
+		tmp.setActivityId(null);
+		tmp.setCreatedTimestamp(new Date());
+		tmp.setInstance(new Instance(instanceId));
+		tmp.setInvokedBy(new CreatedBy(invokedBy));
+		tmp.setType("" + this.id.get());
+		this.id.incrementAndGet();
+		return this.client.postForObject(this.createActivityUrl, tmp, ActivityBoundary.class);
+	}
+
+	public RestTemplate getClient() {
+		return client;
+	}
+
+	public void setClient(RestTemplate client) {
+		this.client = client;
+	}
+
+	public AtomicLong getId() {
+		return id;
+	}
+
+	public void setId(AtomicLong id) {
+		this.id = id;
+	}
+
+	public String getCreateUserUrl() {
+		return createUserUrl;
+	}
+
+	public void setCreateUserUrl(String createUserUrl) {
+		this.createUserUrl = createUserUrl;
+	}
+
+	public String getCreateInstanceUrl() {
+		return createInstanceUrl;
+	}
+
+	public void setCreateInstanceUrl(String createInstanceUrl) {
+		this.createInstanceUrl = createInstanceUrl;
+	}
+
+	public String getCreateActivityUrl() {
+		return createActivityUrl;
+	}
+
+	public void setCreateActivityUrl(String createActivityUrl) {
+		this.createActivityUrl = createActivityUrl;
+	}
+
+}
---
+++ src/test/java/rest_api_tests/user_api/LoginUserTest.java
@@ -0,0 +1,86 @@
+package rest_api_tests.user_api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.annotation.PostConstruct;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import iob.Application;
+import iob.attributes.UserId;
+import iob.boundaries.UserBoundary;
+import iob.data.UserRole;
+import iob.logic.TestingDaoService;
+import iob.logic.TestingFactory;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
+@Profile("Testing")
+public class LoginUserTest {
+
+	@Autowired
+	private TestingDaoService testingService;
+
+	@Autowired
+	private TestingFactory testingFactory;
+
+	private UserId user;
+
+	private RestTemplate client;
+	private String url;
+	private int port;
+
+	@LocalServerPort
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		this.url = "http://localhost:" + this.port + "/iob/users/login/";
+		this.testingFactory.setPort(this.port);
+		this.client = new RestTemplate();
+	}
+
+	@AfterEach
+	public void after() {
+		this.testingService.getUserDao().deleteAll();
+	}
+
+	@Test
+	public void testLoginAdmin() {
+		this.user = testingFactory.createNewUser(UserRole.ADMIN).getUserId();
+		assertThat(this.client.getForObject(this.url + this.user, UserBoundary.class)).isNotNull()
+				.hasSameClassAs(new UserBoundary());
+	}
+
+	@Test
+	public void testLoginManager() {
+		this.user = this.testingFactory.createNewUser(UserRole.MANAGER).getUserId();
+		assertThat(this.client.getForObject(this.url + this.user, UserBoundary.class)).isNotNull()
+				.hasSameClassAs(new UserBoundary());
+	}
+
+	@Test
+	public void testLoginPlayer() {
+		this.user = this.testingFactory.createNewUser(UserRole.PLAYER).getUserId();
+		assertThat(this.client.getForObject(this.url + this.user, UserBoundary.class)).isNotNull()
+				.hasSameClassAs(new UserBoundary());
+	}
+
+	@Test
+	public void testInvalidUserLogin() {
+		UserId invalidUser = new UserId("DOMAIN", "NOTEXIST@EMAIL.COM");
+		assertThrows(HttpClientErrorException.NotFound.class, () -> {
+			this.client.getForObject(this.url + invalidUser, UserBoundary.class);
+		});
+	}
+}
